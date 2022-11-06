84be9d86044af1f6dafd7bb92555d0ce8377fe62
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-06 21:26:51 +0900

    Setting : Add Hot Reload Cli

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000
+  await app.listen(port);
+  console.log(`Listening On Port ${port}`)
+
+  if (module.hot) { // new !
+    module.hot.accept(); // new !
+    module.hot.dispose(() => app.close()); // new !
+  } // new !
 }
 bootstrap();
