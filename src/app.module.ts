c5a53b77ade33af31932c476a813a9a9646b2d3d
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-06 22:53:03 +0900

    feat : Create User Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MorganInterceptor, MorganModule } from 'nest-morgan';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({isGlobal : true}), MorganModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [
+    ConfigService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MorganInterceptor("combined"),
+    },
+  ],
 })
 export class AppModule {}
