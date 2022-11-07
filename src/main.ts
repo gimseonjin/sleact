c878beeb7f969b8da6769560d95ef5252d88006d
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-07 18:46:18 +0900

    setting : Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
@@ -6,12 +7,22 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000
-  await app.listen(port);
-  console.log(`Listening On Port ${port}`)
 
   if (module.hot) { // new !
     module.hot.accept(); // new !
     module.hot.dispose(() => app.close()); // new !
   } // new !
+
+  const config = new DocumentBuilder()
+    .setTitle('SLEACT API')
+    .setDescription('This is for sleact project')
+    .setVersion('1.0')
+    .addTag('sleact')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
+  console.log(`Listening On Port ${port}`)
 }
 bootstrap();
