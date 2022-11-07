a614744cf1133aa9f643b520c5d58bfdc80b07ef
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2022-11-07 18:22:37 +0900

    feat : Create Post User Logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('api/users')
+export class UsersController {
+
+    constructor(private usersService: UsersService){}
+
+    @Get()
+    getUsers(@Req() req){
+        return req.user;
+    }
+
+    @Post()
+    postUser(@Body() data: JoinRequestDto){
+        return this.usersService.postUser(data);
+    }
+
+    @Post()
+    login(@Req() req){
+        return req.user;
+    }
+
+    @Post()
+    logOut(@Req() req, @Res() res){
+        req.logOut();
+        res.clearCokkie('connect.sid', {httpOnly : true});
+        res.send('ok');
+    }
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    public postUser(data: JoinRequestDto){
+        console.log(data)
+        return data
+    }
+}
